fa9b67e9035309c86a8a5e6846ceea85282666f2
baskanbaran601-droid <baskanbaran601@gmail.com>
2026-03-15 16:33:14 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,27 +1,14 @@
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.button import Button
 
 class HesapMakinesi(App):
     def build(self):
-        ana_layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
-
-        # Yapımcılar
-        yapimcilar = Label(
-            text="Yapımcılar: Semih ve Baran",
-            size_hint_y=None,
-            height=100,
-            color=(0, 1, 1, 1),
-            font_size=24
-        )
-        ana_layout.add_widget(yapimcilar)
-
-        # Uygulama Ekranı
-        ekran = Label(text="Hesap Makinesi Hazır", font_size=32)
-        ana_layout.add_widget(ekran)
-        
-        return ana_layout
+        layout = BoxLayout(orientation='vertical')
+        label = Label(text="Yapimcılar: Semih ve Baran")
+        layout.add_widget(label)
+        return layout
 
 if __name__ == '__main__':
     HesapMakinesi().run()
